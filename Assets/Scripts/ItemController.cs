46ce5d304ff6688cad208fd7b19441653322114b
Eric Baker <8997206+patches34@users.noreply.github.com>
2025-11-08 08:43:22 -0500

    Added basic mouse dragging for items

---
+++ Assets/Scripts/ItemController.cs
@@ -0,0 +1,61 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public enum ItemState
+{
+    None,
+    Dragging,
+    Placed
+}
+
+public class ItemController : MonoBehaviour
+{
+    [SerializeField]
+    List<BoxCollider2D> tileColliders;
+
+    Vector2 moveDelta;
+
+    [SerializeField]
+    RectTransform rect;
+
+    [SerializeField]
+    ItemState currentState;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        foreach(BoxCollider2D childCollider in transform.GetComponentsInChildren<BoxCollider2D>())
+        {
+            tileColliders.Add(childCollider);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (currentState)
+        {
+            case ItemState.None:
+                break;
+            case ItemState.Dragging:
+                FollowMouse();
+                break;
+            case ItemState.Placed:
+                break;
+        }
+    }
+
+    void FollowMouse()
+    {
+        //  Check if player would move outside the bounds
+        Vector2 newPos = Mouse.current.position.ReadValue();
+
+        rect.anchoredPosition = newPos;
+    }
+
+    void OnStartDrag()
+    {
+        rect = transform.parent.GetComponent<RectTransform>();
+    }
+}
