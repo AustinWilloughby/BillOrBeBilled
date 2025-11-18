48be094c6993b2b46f7346433d2689434d8b9381
Eric Baker <8997206+patches34@users.noreply.github.com>
2025-11-18 15:45:10 -0500

    Fixed picking up objects with multiple colliders

--- Assets/Scripts/ObjectGrabber.cs
+++ Assets/Scripts/ObjectGrabber.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,6 +10,7 @@ public class ObjectGrabber : MonoBehaviour
     [SerializeField] float pickupRadius;
 
     Collider[] pickupsInFront;
+    List<PropItemData> inFrontItems = new List<PropItemData>();
 
     [SerializeField]
     InventorySO inventorySO;
@@ -17,21 +19,30 @@ public class ObjectGrabber : MonoBehaviour
     {
         Vector3 position = transform.position + (transform.forward * distInFront);
         position += transform.up * (-1.0f * distDown);
+
         pickupsInFront = Physics.OverlapSphere(position, pickupRadius, pickupMask);
+
+        inFrontItems.Clear();
+        foreach(Collider collider in pickupsInFront)
+        {
+            PropItemData pickupItem = collider.transform.root.GetComponent<PropItemData>();
+
+            if(pickupItem != null && !inFrontItems.Contains(pickupItem))
+            {
+                inFrontItems.Add(pickupItem);
+            }            
+        }
     }
 
     public void OnClick(InputAction.CallbackContext ctx)
     {
         if(ctx.phase == InputActionPhase.Performed)
         {
-            foreach(Collider c in pickupsInFront)
+            foreach(PropItemData pickupItem in inFrontItems)
             {
-                //TODO: Add to inventory
-                inventorySO.uiInventoryManager.PickupItemOfType(c.transform.root.GetComponent<PropItemData>().itemType);
+                inventorySO.uiInventoryManager.PickupItemOfType(pickupItem.itemType);
 
-
-
-                Destroy(c.transform.root.gameObject);
+                Destroy(pickupItem.gameObject);
             }
         }
     }
